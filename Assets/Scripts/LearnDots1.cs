9ccf707d5385f4670bb6aded93454639968ea3d4
youmangu <873211279@qq.com>
2021-03-08 16:32:02 +0800

    +[Scripts]dots 的 ECS 简单使用

---
+++ Assets/Scripts/LearnDots1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class LearnDots1 : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float printData;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new PrinDataCompnet() { mPrintData = printData });
+    }
+}
---
+++ Assets/Scripts/PrintDataSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class PrintDataSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref PrinDataCompnet printdata) =>
+        {
+            Debug.Log(printdata.mPrintData);
+        });
+
+    }
+}
