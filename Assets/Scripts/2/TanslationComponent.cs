682bbeaa1fc7f6b90920c2b6b7fd5313136027ad
youmangu <873211279@qq.com>
2021-03-08 20:20:58 +0800

    +[Scripts]dots 的 组件 简单使用

---
+++ Assets/Scripts/2/TanslationComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+[DisableAutoCreation]
+public class TanslationComponent : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation) => {
+            translation.Value = new Unity.Mathematics.float3(1, 1, 1);
+        }); 
+    }
+
+}
---
+++ Assets/Scripts/3/RatationAulerXYZSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class RatationAulerXYZSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref RotationEulerXYZ rotationEuler) => {
+            rotationEuler.Value = new Unity.Mathematics.float3(0, 45, 0);
+        }); 
+    }
+
+}
